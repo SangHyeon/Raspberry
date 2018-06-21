cf82b6b2ce9c7135e29e4fdfb9c7f2ff06ce43c2
Jason Tilley <jason@ambitionenterprise.ca>
2018-06-20 23:41:29 -0700

    Update blink.py
    Added a try / except to catch KeyboardInterrupt

--- EX_GPIO/blink_py/blink.py
+++ EX_GPIO/blink_py/blink.py
@@ -1,11 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
+# Define LED pin
+LED = 20
 GPIO.semode(GPIO.BCM)
-GPIO.setuo(20, GPIO.OUT)
+GPIO.setuo(LED, GPIO.OUT)
 
 while True: #infinity loop
-	GPIO.output(25, GPIO.HIGH) #pin 25 led on
-	time.sleep(1)
-	GPIO.output(25, GPIO.LOW)#pin 25 led off
-	time.sleep(1)
+	# Wrap this in a try / except
+	try:
+		print("###> LED Status: ON")
+		GPIO.output(25, GPIO.HIGH) #pin 25 led on
+		time.sleep(1)
+		
+		print("###> LED Status: OFF")
+		GPIO.output(25, GPIO.LOW)#pin 25 led off
+		time.sleep(1)
+		
+	# Handle user exit // Ctrl-C
+	except KeyboardInterrupt:
+		GPIO.output(25, GPIO.LOW) # Turn off the LED when exiting via the keyboard
+		sys.exit()
